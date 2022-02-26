3266b67fa06b3d6b440959ea0067acade5505638
Flc <four_leaf_clover@foxmail.com>
2022-02-26 16:02:57 +0800

    cmd init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/flc1125/event
+
+go 1.16
+
+require github.com/spf13/cobra v1.3.0
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/flc1125/event/cmd"
+
+func main() {
+	cmd.Execute()
+}
