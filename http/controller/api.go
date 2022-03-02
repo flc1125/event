95206c03086b7c6907df5ed76fdee0a26ffbe0b8
Flc <four_leaf_clover@foxmail.com>
2022-03-02 14:22:44 +0800

    引入template

--- http/controller/api.go
+++ http/controller/api.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 var ApiController = &apiController{}
@@ -9,7 +10,10 @@ var ApiController = &apiController{}
 type apiController struct{}
 
 func (this *apiController) Ping(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
+	//c.JSON(200, gin.H{
+	//	"message": "pong",
+	//})
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"title": "Pong",
 	})
 }
--- http/template.go
+++ http/template.go
@@ -1,1 +1,5 @@
 package http
+
+func init() {
+	engine.LoadHTMLGlob("resource/views/*")
+}
